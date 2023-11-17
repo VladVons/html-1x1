5610decd562fe60efc112bb3aef81c6b460870ab
VladVons <vladvons@gmail.com>
2023-11-17 08:43:26 +0200

    MyCommit

--- src/assets/js/timer_modal.js
+++ src/assets/js/timer_modal.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var Min = 2 * 1000;
-    var Max = 3 * 1000;
+    var Min = 20 * 1000;
+    var Max = 30 * 1000;
     Delay = Math.random() * (Max - Min) + Min;
     setTimeout(function() {
         var Modal = new bootstrap.Modal(document.getElementById('vTimerModal'));
