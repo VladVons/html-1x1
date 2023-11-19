2a41ca15c3acec12f4f7400ccba18dd9347b988e
VladVons <vladvons@gmail.com>
2023-11-19 08:54:13 +0200

    MyCommit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,11 +6,13 @@ function showTooltip(aMsg, aId = null) {
         const element = document.getElementById(aId)
         const rect = element.getBoundingClientRect()
 
+        /*
         // ToDo. extra width
         tooltip.style.left = rect.left + 'px'
         tooltip.style.top = (rect.top + rect.height) + 'px'
         tooltip.style.right = 0
         console.log(tooltip.style.width)
+        */
     }
 
     document.body.appendChild(tooltip);
