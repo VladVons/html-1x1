2731f1d939924e3fb35e58f584d5cf2806b5d137
VladVons <vladvons@gmail.com>
2023-11-19 09:42:12 +0200

    MyCommit

---
+++ src/misc/css_optimize.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# VladVons, 2023.11.19
+
+
+DirDst="build/css"
+mkdir -p $DirDst
+
+
+Purge() {
+    DirSrc="../assets/vendor/css"
+
+    purgecss \
+    --css $DirSrc/bootstrap.css $DirSrc/fontawesome.css \
+    --content ../*.html \
+    --output $DirDst
+}
+
+Combine()
+{
+    DirSrc="../assets/css"
+    FileSrc="$DirSrc/styles.css"
+    FileDst="$DirDst/styles.css"
+
+    echo > $FileDst
+    grep -o '".*"' $FileSrc | sed 's/"//g' |\
+    while read x; do
+        echo $x
+        echo -e "\n/* file: $x */" >> $FileDst
+        cat $DirSrc/$x >> $FileDst
+    done
+}
+
+Purge
+Combine
