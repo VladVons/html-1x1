ecee4f84374ebe7393341cea3d73c34598b65073
VladVons <vladvons@gmail.com>
2023-12-19 11:15:23 +0200

    admin-undoChanges

--- src/assets/common/js/lib.js
+++ src/assets/common/js/lib.js
@@ -45,6 +45,94 @@ class TDict {
     }
 }
 
+class TFormChangeTracker {
+    constructor(aFormId, aCheckedName) {
+        this.checkedName = aCheckedName
+        this.initialValues = {}
+
+        this.form = document.getElementById(aFormId)
+        this.init()
+    }
+
+    getAttr(aElement, aName = 'name') {
+        // more simple access is just aElement.name but here is more flexable by str
+        return aElement[aName]
+    }
+        
+    filter(aElement) {
+        return (['text', 'number', 'checkbox'].includes(aElement.type) || aElement.tagName === 'SELECT') && this.getAttr(aElement)
+    }
+
+    getInputs() {
+        let Res = []
+        Array.from(this.form.elements).forEach((element) => {
+            if (this.filter(element)) {
+                Res.push(element)
+            }
+        })
+        return Res
+    }
+
+    init() {
+        for (const x of this.getInputs()) {
+            this.initialValues[this.getAttr(x)] = this.getValue(x)
+        }
+
+        this.form.addEventListener('input', (event) => {
+            const element = event.target
+
+            if (this.filter(element)) {
+                const initialValue = this.initialValues[this.getAttr(element)]
+                const currentValue = this.getValue(element)
+
+                if (currentValue !== initialValue) {
+                    element.classList.add(this.checkedName)
+                } else {
+                    element.classList.remove(this.checkedName)
+                }
+            }
+        })
+    }
+
+    getChanges() {
+        let Res = {}
+        for (const x of this.getInputs()) {
+            const value = this.getValue(x)
+            if (this.initialValues[this.getAttr(x)] != value) {
+                Res[this.getAttr(x)] = value
+            }
+        }
+        return Res
+    }   
+
+    undoChanges() {
+        for (const x of this.getInputs()) {
+            this.setValue(x, this.initialValues[this.getAttr(x)])
+        }
+    }
+    
+    getValue(aElement) {
+        if (aElement.type == 'checkbox') {
+            return aElement.checked.toString()
+        } else if (aElement.tagName == 'SELECT') {
+            return aElement.options[aElement.selectedIndex].value
+        } else {
+            return aElement.value
+        }
+    }
+
+    setValue(aElement, aValue) {
+        if (aElement.type == 'checkbox') {
+            aElement.checked = aValue
+        } else if (aElement.tagName == 'SELECT') {
+            aElement.options[aElement.selectedIndex].value = aValue
+        } else {
+            aElement.value = aValue
+        }
+    }
+
+}
+
 function format(aPattern, aValues) {
     return aPattern.replace(/\{(\w+)\}/g, (match, key) => aValues[key] || match)
 }
